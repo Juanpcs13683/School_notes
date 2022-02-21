9604155693345e611856e413ee2776274cf998c0
Juanpcs13683 <84336310+Juanpcs13683@users.noreply.github.com>
2022-02-21 16:05:49 -0500

    Actualizacion metodo crear notas
    Se creo un metodo llamado nota que se encarga de crear las notas y guardarlas y asi solo llamarlo en el switch case de cada materia

--- src/ScoreController.java
+++ src/ScoreController.java
@@ -15,6 +15,31 @@ public class ScoreController {
 
     //lista de nombres
 
+
+    //metodo que crea las notas
+    public void nota(String nombreMAteria){
+        try {
+            File creador = new File("src/Notas/" + nombreMateria + ".csv");
+            PrintWriter escritor = new PrintWriter(creador);
+            escritor.println("Nombre,Materia,Nota1,Nota2,Nota3,Nota4,Definitiva");
+            for (String estudiante : lista_nombres) {
+                JOptionPane.showMessageDialog(null, "Crear Nota\n" +
+                        "Por favor ingrese los datos a continuacion");
+
+                String nota1 = JOptionPane.showInputDialog(null, "Estudiante: " + estudiante + "\n" + "Nota 1");
+                String nota2 = JOptionPane.showInputDialog(null, "Estudiante: " + estudiante + "\n" + "Nota 2");
+                String nota3 = JOptionPane.showInputDialog(null, "Estudiante: " + estudiante + "\n" + "Nota 3");
+                String nota4 = JOptionPane.showInputDialog(null, "Estudiante: " + estudiante + "\n" + "Nota 4");
+                String definitiva = String.valueOf(materia.calculaDef(Integer.parseInt(nota1), Integer.parseInt(nota2),
+                        Integer.parseInt(nota3), Integer.parseInt(nota4)));
+                escritor.println(estudiante+ "," +nombreMateria + ","+nota1+","+nota2+","+nota3+","+nota4+","+definitiva);
+            }
+            escritor.close();
+        }catch (FileNotFoundException ex){
+            JOptionPane.showMessageDialog(null, "Error al crear notas de "+nombreMAteria);
+        }
+    }
+
     //2.1 crearNotas
     public void crearNotas(){
         //creando lista de nombres creados
@@ -28,51 +53,26 @@ public class ScoreController {
         while(sc.hasNext()){
             lista_nombres.add(sc.nextLine());
         }
-        boolean activo = true;
-        while(activo) {
+
+
+        while(nombreMateria=="") {
             switch (JOptionPane.showInputDialog(null, "Seleccione una materia\n" +
                     "1. Matematicas\n" + "2. Fisca\n" + "3. Español\n" + "4. Sociales")) {
                 case "1":
                     nombreMateria = "Matematicas";
-
-                           try {
-                               File creador = new File("src/Notas/" + nombreMateria + ".csv");
-                               PrintWriter escritor = new PrintWriter(creador);
-                               escritor.println("Nombre,Materia,Nota1,Nota2,Nota3,Nota4,Definitiva");
-                               for (String estudiante : lista_nombres) {
-                                   JOptionPane.showMessageDialog(null, "Crear Nota\n" +
-                                           "Por favor ingrese los datos a continuacion");
-
-                                   String nota1 = JOptionPane.showInputDialog(null, "Estudiante: " + estudiante + "\n" + "Nota 1");
-                                   String nota2 = JOptionPane.showInputDialog(null, "Estudiante: " + estudiante + "\n" + "Nota 2");
-                                   String nota3 = JOptionPane.showInputDialog(null, "Estudiante: " + estudiante + "\n" + "Nota 3");
-                                   String nota4 = JOptionPane.showInputDialog(null, "Estudiante: " + estudiante + "\n" + "Nota 4");
-                                   String definitiva = String.valueOf(materia.calculaDef(Integer.parseInt(nota1), Integer.parseInt(nota2),
-                                           Integer.parseInt(nota3), Integer.parseInt(nota4)));
-                                   escritor.println(estudiante+ "," +nombreMateria + ","+nota1+","+nota2+","+nota3+","+nota4+","+definitiva);
-                               }
-                               escritor.close();
-                           }catch (FileNotFoundException ex){
-
-                           }
-
-
-                    activo = false;
+                    nota(nombreMateria);
                     break;
                 case "2":
                     nombreMateria = "Fisica";
-
-                    activo = false;
+                    nota(nombreMateria);
                     break;
                 case "3":
                     nombreMateria = "Español";
-
-                    activo = false;
+                    nota(nombreMateria);
                     break;
                 case "4":
                     nombreMateria = "Soiales";
-
-                    activo = false;
+                    nota(nombreMateria);
                     break;
                 default:
                     JOptionPane.showInputDialog(null, "Opcion no valida");
